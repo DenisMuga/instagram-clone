f152cb632b9eb379d3c45f942d3705bcfa719f92
DenisMuga <denismugah5@gmail.com>
2022-06-04 14:50:15 +0300

    Creating index and profile functions in the views py

--- zetu/views.py
+++ zetu/views.py
@@ -29,6 +29,27 @@ def signup(request):
     return render(request, 'registration/signup.html', {'form': form})
 
 @login_required(login_url='login')
+def index(request):
+    images = Post.objects.all()
+    users = User.objects.exclude(id=request.user.id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user.profile
+            post.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = PostForm()
+    context = {
+        'images': images,
+        'form': form,
+        'users': users,
+
+    }
+    return render(request, 'plata/index.html', context)
+
+@login_required(login_url='login')
 def profile(request, username):
     images = request.user.profile.posts.all()
     if request.method == 'POST':
