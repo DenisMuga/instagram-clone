df2e6a7f13fd620261c3af07c4882adc90181c42
DenisMuga <denismugah5@gmail.com>
2022-06-04 12:45:23 +0300

    Creating a sign up function in the views py

--- zetu/views.py
+++ zetu/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render,redirect,get_list_or_404
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = RegForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            f_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=f_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegForm()
+    return render(request, 'registration/signup.html', {'form': form})
