4872960db7ba0882a71739d7823a7be313a007bc
DenisMuga <denismugah5@gmail.com>
2022-06-04 14:48:03 +0300

    Importing various classes from modules

--- zetu/views.py
+++ zetu/views.py
@@ -1,5 +1,15 @@
+from multiprocessing import context
 from django.shortcuts import render,redirect,get_list_or_404
+from django.http import HttpResponseRedirect, JsonResponse
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
+from .models import Post, Comment, Profile, Follow
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
+from django.template.loader import render_to_string
+from django.views.generic import RedirectView
 from .forms import RegForm,PostForm,UpdateUserProfileForm,UpdateUserForm,CommentForm
 
 # Create your views here.
@@ -17,3 +27,25 @@ def signup(request):
     else:
         form = RegForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+@login_required(login_url='login')
+def profile(request, username):
+    images = request.user.profile.posts.all()
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateUserProfileForm(instance=request.user.profile)
+    context = {
+        'user_form': user_form,
+        'prof_form': profile_form,
+        'images': images,
+
+    }
+    return render(request, 'plata/profile.html', context)
+
